ba110298d924c63124a73bdaef068be4ced5ce2b
lixuannan <leecodingcow@gmail.com>
2024-08-17 16:02:53 +0800

    Version 0.1.2

--- lib/VOIM.py
+++ lib/VOIM.py
@@ -2,7 +2,6 @@
 
 import sys
 import os
-import webbrowser
 import subprocess
 
 
@@ -11,6 +10,7 @@ def compile(filename: str, compiler: str) -> int:
     output, error = pro.communicate()
     exit_code = pro.returncode
     print(f"{output.decode('utf-8')}\n{error.decode('utf-8')}\n")
+    return exit_code
 
 
 def run(cmd):
@@ -18,7 +18,7 @@ def run(cmd):
     output, error = pro.communicate()
     exit_code = pro.returncode
     print(f"{output.decode('utf-8')}\n{error.decode('utf-8')}")
-    print(f"Process ended with code: {exit_code}")
+    print(f"Process ended with code {exit_code}")
 
 
 filename = sys.argv[1]
@@ -26,16 +26,20 @@ filetype = filename.split('.')[-1]
 
 
 if filetype == 'cpp' or filetype == 'cc':
-    compile(filename, "g++")
-    run(f"./{filename}.out")
+    returncode = compile(filename, "g++")
+    if returncode != 0:
+        print("Compile Error, won't run")
+    else:
+        run(f"./{filename}.out")
 elif filetype == 'py':
     run(f"python {filename}")
 elif filetype == 'c':
-    compile(filename, "gcc")
-    run(f"./{filename}.out")
-elif filetype == 'md':
-    run(f"pandoc -f markdown -t html5 --css /home/codingcowlee/github.css -s -o {filename}.html {filename}")
-    webbrowser.open(f"{filename}.html")
-
+    returncode = compile(filename, 'cc')
+    if returncode != 0:
+        print("Compile Error, code will not run")
+    else:
+        run(f"./{filename}.out")
+else:
+    print("Unsupport filetype detected, run failed")
 
 
--- plugin/VOIM.vim
+++ plugin/VOIM.vim
@@ -1,5 +1,5 @@
-map <F5> :!VOIM % <CR>
-imap <F5> <esc> :!VOIM % <CR>
-vmap <F5> <esc> :!VOIM % <CR>
+map <F5> :!../lib/VOIM.py % <CR>
+imap <F5> <esc> :!../lib/VOIM.py % <CR>
+vmap <F5> <esc> :!../lib/VOIM.py % <CR>
 
 
